2443fbaaa34df9083bf7fe597e37a9f42ef123f6
chrisq557 <chrisq557santos@gmail.com>
2025-07-10 11:32:40 +0100

    Install gunicorn 23.0.0 package and add it to requierements.txt file. Create Procfile file and add content. Add heroku hostname and 127.0.0.1 to ALLOWED_HOSTS list. Change DEBUG to False

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ke2%&nn#*9%uv=4o7q7f16hw#(d@#b_i!yere_5yorhs_41l!v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
